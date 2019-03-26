703a8d140b13dc23d0ec47832d9f6bb88260a874
Danie van der Bank <vanderbankdanie@gmail.com>
2019-03-26 12:09:52 +0200

    add name and nickname to databind

--- app/src/main/java/android/example/aboutme/MainActivity.kt
+++ app/src/main/java/android/example/aboutme/MainActivity.kt
@@ -1,7 +1,6 @@
 package android.example.aboutme
 
 import android.content.Context
-import android.databinding.DataBindingUtil
 import android.example.aboutme.databinding.ActivityMainBinding
 import android.os.Bundle
 import android.view.View
@@ -10,22 +9,27 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private  val myName: MyName = MyName("Danie van der Bank")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        binding.myName = myName
         binding.doneButton.setOnClickListener{(addNickname(it))}
     }
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text
+            //nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
---
+++ app/src/main/java/android/example/aboutme/MyName.kt
@@ -0,0 +1,5 @@
+package android.example.aboutme
+
+import android.provider.ContactsContract
+
+data class MyName(var name: String = "", var nickname: String = "")
